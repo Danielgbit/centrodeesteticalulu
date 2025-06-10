332834d5810a52a69ee77331449254d633612701
danielgbit <danielbitmobb@gmail.com>
2025-06-10 10:20:42 -0500

    Adjust the heigth of the ProductCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <div className="w-[280px] h-[520px] flex flex-col justify-between items-center bg-card rounded-lg shadow">
+    <div className="w-[280px] h-[530px] flex flex-col justify-between items-center bg-card rounded-lg shadow">
       <div className="bg-white w-full">
         <Image
           alt={product.name}
@@ -13,13 +13,13 @@ const ProductCard = ({ product }: { product: Product }) => {
         />
       </div>
       <div>
-        <h3 className="text-light pt-4 px-2 text-center tracking-text h-20 text-base font-bold">
+        <h3 className="text-center text-light pt-4 px-2 flex items-center justify-center tracking-text h-15 text-sm font-bold">
           {product.name}
         </h3>
-        <p className="pt-3 px-3 h-20 text-center text-sm font-normal text-colorMedium tracking-text ">
+        <p className="text-center pt-3 px-3 h-15 flex items-center justify-center text-xs font-normal text-colorMedium tracking-text ">
           {product.description}
         </p>
-        <p className="py-2 price text-center h-15 text-sm text-white font-bold ">
+        <p className="text-center py-2 price flex items-center justify-center h-15 text-sm text-white font-bold ">
           <span className="tracking-text">$ </span>
           {product.price.toLocaleString("es-CO")}
         </p>
